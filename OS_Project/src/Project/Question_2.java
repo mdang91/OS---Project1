b3bc4ad00b809f03938a78a986596b6e1204ac0c
mdang91 <minhdtq2208@gmail.com>
2022-03-25 02:57:12 -0400

    Update Question_2.java

--- OS_Project/src/Project/Question_2.java
+++ OS_Project/src/Project/Question_2.java
@@ -2,6 +2,7 @@ package Project;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question_2 {
@@ -72,53 +73,25 @@ public class Question_2 {
 		for(int i = 0; i < num; i++) {
 			proc = sched.getProcess(i);
 			sumC += proc.getCycles();
-			sumM = proc.getMem();
+			sumM += proc.getMem();
 			
 		}
 		
 		averageC = sumC / num;
 		averageM = sumM / num;
 		runtime = averageC / sched.getSpeed();
-				
+		
+		System.out.println("Number of process:  " + num);
 		System.out.println("Average Cycles:  " + averageC);
 		System.out.println("Average Runtime: " + runtime + " seconds");
 		System.out.println("Average Memory:  " + averageM);
 	}
 	
-public static void RRSched(Schedule sched, Schedule master, long burstTime) {
 	
-		
-	long low = sched.getProcess(0).getCycles();
-	int lowIndex = 0;
-	Schedule sorted = new Schedule("SJF");
+	public static void RRSched(Schedule sched, Schedule master, long burstTime) {
 	
-	//while there still processes in sched
-	while(sched.getNum() > 0) {
-		//for each process still in sched
-		for(int i = 0; i<sched.getNum(); i++) {
-			//if current process is shorter than low process
-			if(sched.getProcess(i).getCycles() < low) {
-				//move new process to low
-				low = sched.getProcess(i).getCycles();
-				//save index
-				lowIndex = i;
-			}
-		}
-		//add the shortest process to the sorted schedule
-		sorted.addProcess(sched.getProcess(lowIndex));
-		//remove the shortest process from sched
-		sched.removeProcess(lowIndex);
-		
-		//if there's more to sort , resets low and lowIndex
-		if(sched.getNum() > 0) {
-			low = sched.getProcess(0).getCycles();
-			lowIndex = 0;
-		}
-		
-	}
-
-		
 		//sched.printSchedule();
+		//master.printSchedule();
 		//split the CPUS by half 3 have 2Ghz and 3 have 4 GHz
 		Schedule PA = new Schedule("CPU A");
 		PA.setSpeed(2000000000L);		
@@ -138,15 +111,17 @@ public static void RRSched(Schedule sched, Schedule master, long burstTime) {
 		Process proc; 
 		//distributes each process to a CPU,
 		//Starts with CPU PA and goes around
-		for(int i = 0; i < 250; i++) {
+		   
+		 for(int i = 0; i < sched.getNum(); i++) {		
 			
-			//System.out.println("Bien:" + i);
+			proc = sched.getProcess(i);
 			
-			proc = sorted.getProcess(i);
-			
-			
-			if(i<125) {
-			
+			if(i == sched.getNum()) {
+				cpu = "D";
+			}
+			if(i < sched.getNum()/2)
+			{
+
 				if(cpu == "A") {
 					PA.addProcess(proc);
 					cpu = "B";
@@ -159,22 +134,23 @@ public static void RRSched(Schedule sched, Schedule master, long burstTime) {
 				PC.addProcess(proc);
 				cpu = "A";
 				}
-			}
-			else {
-				cpu = "D";
-				 if(cpu == "D") {
+			}	
+			if(i > sched.getNum()/2) {
+				
+					if(cpu == "D") {
 					PD.addProcess(proc);
 				cpu = "E";
 				}
-			else if(cpu == "E") {
-				PE.addProcess(proc);
+				 else if(cpu == "E") {
+					 PE.addProcess(proc);
 				cpu = "F";
-			}
-			else if(cpu == "F") {
-				PF.addProcess(proc);
+				 }
+				 else if(cpu == "F") {
+					 PF.addProcess(proc);
 				cpu = "D";
-			}
-			}
+				 }
+				 }
+
 		
 		}
 		/*now all the cpus have a split of the processes
@@ -210,13 +186,16 @@ public static void RRSched(Schedule sched, Schedule master, long burstTime) {
 		int waitAvg = waitSum / master.getNum();
 		int tatAvg = tatSum / master.getNum();
 		
-		System.out.println("RR:");
+		System.out.println("Round Robin Optimize");
+		System.out.println("Number of process:  " + master.getNum());
+		System.out.println("Number of scheduled process:  " + sched.getNum());
 		System.out.println("Average wait time: " + waitAvg);
 		System.out.println("Average turn-around time: " + tatAvg);
 		System.out.println();
 		
 		
 	}
+	
 	public static void RR(Schedule master) {
 		Schedule mCopy = new Schedule("Master Copy");
 		mCopy = master.copySchedule();
@@ -245,7 +224,7 @@ public static void RRSched(Schedule sched, Schedule master, long burstTime) {
 		int i = 0;
 		
 		
-		System.out.println(master.getNum() + "  " + mCopy.getNum() + "  ");	
+		//System.out.println(master.getNum() + "  " + mCopy.getNum() + "  ");	
 		while(!done) {
 			
 
